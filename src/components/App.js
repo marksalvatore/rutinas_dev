bd0ea0f31af5f28e1c1cfc6127b63f6d4399a9bc
Mark Salvatore <mark@salvatore.us>
2017-04-20 10:16:57 -0400

    Make app open to GetStarted if newUser, else Routines

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router';
 
-import eightball from '../../public/images/eightball.png';
+import GetStarted from './GetStarted';
+import Routines from './Routines';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="Page">
+  constructor() {
+    super();
 
-        <div className="App-title">Routines</div>
-        <div className="App-byline">Improve your pool game.</div>
-        <div className="App-logo">
-          <img src={eightball} alt="Eight Ball" width="100"/>
-        </div>
-        <div className="App-description">
-          Create practice routines from the drills
-          <br/>you choose, and monitor your progress.
-        </div>
-
-        <button className="getStarted">
-           <Link to="/info">Get started</Link>
-        </button>
-        
-      </div>
-    );
+    this.isNewUser = this.isNewUser.bind(this);
   }
+
+  isNewUser() {
+    // if there are no drills saved to storage, this is a "new" user
+    if( !localStorage.getItem('drills')) {
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    if ( this.isNewUser() ) {
+       return <GetStarted />
+    } else {
+      return <Routines />
+    }
+  }
+
 }
 
 export default App;
---
+++ src/components/GetStarted.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import eightball from '../../public/images/eightball.png';
+
+class GetStarted extends React.Component {
+
+  render() {
+
+    return (
+      <div className="Page">
+
+        <div className="App-title">Routines</div>
+        <div className="App-byline">Improve your pool game.</div>
+        <div className="App-logo">
+          <img src={eightball} alt="Eight Ball" width="100"/>
+        </div>
+        <div className="App-description">
+          Create practice routines from the drills
+          <br/>you choose, and monitor your progress.
+        </div>
+
+        <button className="getStarted">
+           <Link to="/info">Get started</Link>
+        </button>
+        
+      </div>
+    );
+  }
+}
+
+export default GetStarted;
