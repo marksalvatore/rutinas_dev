3a44b139cd00188940b004a43a061e36939ece4c
Mark Salvatore <mark@salvatore.us>
2017-05-08 15:14:35 -0400

    Possible future  Nav changes commented out for now

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,14 +4,20 @@ import { Link } from 'react-router';
 import '../css/Nav.css';
 
 const Nav = () => {
-    return (
-        <nav className="Nav">
-            <Link to="/info"><div>Info</div></Link>
-            <Link to="/stats"><div>Stats</div></Link>
-            <Link to="/routines"><div>Routines</div></Link>
-            <Link to="/new"><div>+ New</div></Link>
-        </nav>
-    );
+
+	//const logo = "https://pad.chalkysticks.com/dbcb7.svg";
+	//const infoIcon = "//upload.wikimedia.org/wikipedia/en/thumb/3/35/Information_icon.svg/120px-Information_icon.svg.png";
+
+  return (
+      <nav className="Nav">
+          <Link to="/info"><div>Info</div></Link>
+          {/*<Link to="/info"><img src={infoIcon} width="50" alt="Rutinas logo"/></Link>*/}
+          
+          <Link to="/stats"><div>Stats</div></Link>
+          <Link to="/routines"><div>Routines</div></Link>
+          <Link to="/new"><div>+ New</div></Link>
+      </nav>
+  );
 
 }
 
