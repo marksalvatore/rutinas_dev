630a1b67aaf39278a9c39eae638da8a427b55dcf
Mark Salvatore <mark@salvatore.us>
2017-05-09 10:03:34 -0400

    Remove sort() from NewRoutine

--- src/components/NewRoutine.js
+++ src/components/NewRoutine.js
@@ -204,7 +204,7 @@ class NewRoutine extends React.Component {
   render() {
 
     let titles = this.getTitlesForSelect();
-    let routinesCount = this.routinesCount().sort();
+    let routinesCount = this.routinesCount();
 
     return (
       <NewRoutineRender 
