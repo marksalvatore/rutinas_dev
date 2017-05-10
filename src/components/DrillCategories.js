e0ecf90c43fb185ef1060810bcbbfab63e520a6b
Mark Salvatore <mark@salvatore.us>
2017-05-10 12:10:16 -0400

    Adjust colors for DrillCategories buttons

--- src/components/DrillCategories.js
+++ src/components/DrillCategories.js
@@ -4,13 +4,19 @@ import '../css/DrillCategories.css';
 
 const DrillCategories = (props) => {
 	/* called by NewRoutineRender */
+	console.log('categories:', props.categories);
+	console.log('props.selectedCategory:', props.selectedCategory);
 
   return (
 
 		<ul className="DrillCategories">
+			<li key="9999"><button className={props.selectedCategory === 'all' ? 'category-on' : null} onClick={(e) => props.updateCategory(e)} data-category="all">All</button></li>
 		  { props.categories.map((cat, index) => {
-		    return <li key={index}><button className={props.selectedCategory === cat ? 'category-on' : null} onClick={(e) => props.updateCategory(e)} data-category={cat}>{cat}</button></li>
-		  }) }
+			    return (
+				    	<li key={index}><button className={props.selectedCategory === cat ? 'category-on' : null} onClick={(e) => props.updateCategory(e)} data-category={cat}>{cat}</button></li>
+			  	);
+		  	}) 
+			}
 		</ul>
 
   );
